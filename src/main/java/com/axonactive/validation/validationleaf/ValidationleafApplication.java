f6f9e01e2c651bf60f3ab5f48dafc80362ef1d6f
chang chang <changhammer5499@gmail.com>
2023-08-09 14:33:21 +0700

    create Spring MVC Thymeleaf

---
+++ src/main/java/com/axonactive/validation/validationleaf/ValidationleafApplication.java
@@ -0,0 +1,13 @@
+package com.axonactive.validation.validationleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axonactive/validation/validationleaf/ValidationleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axonactive.validation.validationleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
